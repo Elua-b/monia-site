a1ec55775f3b64762f6c78d9607c9ac637256915
Elua-b <bugingoeloi@gmail.com>
2025-08-14 15:32:12 +0200

    test

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -6,5 +6,5 @@ if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL is not set');
 }
 
-const sql = neon(process.env.DATABASE_URL);
+const sql = neon(process.env.DATABASE_URL) as any;
 export const db = drizzle(sql, { schema });
